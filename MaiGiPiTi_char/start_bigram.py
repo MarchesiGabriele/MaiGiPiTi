c132e1384b5da2dbcb6be775fae8f2eb13388eae
Marchesi Gabriele <marchesigabriele2819@gmail.com>
2025-06-09 18:36:33 +0200

    added log likelihood

--- MaiGiPiTi_char/start_bigram.py
+++ MaiGiPiTi_char/start_bigram.py
@@ -17,23 +17,45 @@ for name in names:
     for ch1, ch2 in zip(chs, chs[1:]): # creo le combinazioni tra lettere successive per ciascuna parola
         N[stoi[ch1],stoi[ch2]] += 1 # ogni volta che ho una coppia di lettere la salvo
 
-
 # normalizzo ciascuna riga
 p = N.float() / N.float().sum(dim=1, keepdim=True)
 
-
-
 newnames = []
 idx = 0
+gen = torch.Generator().manual_seed(2839232938298)
 for seed in range(10):
     name = "" 
-    gen = torch.Generator().manual_seed(seed)
     while True: 
         idx = torch.multinomial(p[idx], 1, replacement=True, generator=gen).item() # estraggo token successivo
         name += itos[idx]
         if idx == 0:
             break
     newnames.append(name)
-    
-
 print(newnames)
+
+
+##### COMPUTING LOG-LIKELIHOOOD
+n = 0
+loglike = 0.0 
+for name in names[:3]:
+    chs = ['.'] + list(name) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]): # creo le combinazioni tra lettere successive per ciascuna parola
+        prob = p[stoi[ch1],stoi[ch2]]
+        logprob = torch.log(prob)
+        n += 1
+        print(f"{ch1}, {ch2}, : {prob:.4f}, {logprob:.4f}")
+        loglike += logprob
+
+# make it negative
+print(f"LOGLIKE: ", f"{-loglike}")
+
+
+
+
+
+
+
+
+
+
+
