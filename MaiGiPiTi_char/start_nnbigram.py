d295dd2846e11f63ba12c267ac248728fdc4b4a5
marchesigabriele <marchesigabriele2819@gmail.com>
2025-07-09 17:03:48 +0200

    finished nn bigram

--- MaiGiPiTi_char/start_nnbigram.py
+++ MaiGiPiTi_char/start_nnbigram.py
@@ -12,7 +12,7 @@ itos = {i:s for s,i in stoi.items()}
 xs,ys = [],[]
 
 N = torch.zeros((len(chars)+1,len(chars)+1), dtype=torch.int32)
-for name in names[:1]:
+for name in names:
     chs = ['.'] + list(name) + ['.']
     for ch1, ch2 in zip(chs, chs[1:]): 
         N[stoi[ch1],stoi[ch2]] += 1 
@@ -21,16 +21,44 @@ for name in names[:1]:
 
 x = torch.tensor(xs)
 y = torch.tensor(ys)
-
+l = len(x) # numer of inputs
 
 import torch.nn.functional as F
 
-xenc = F.one_hot(x, num_classes=len(chars)).float()
+# initialize weigths to random
+W = torch.randn(28,28, requires_grad=True)  # len(chars) = num of inputs
 
-# len(chars) è il numero di input, 
-# inizializzo i pesi del primo layer (questa dimensione di matrice serve moltiplicare tutti gli input per tutti i neuroni\)
-W = torch.randn(len(chars),len(chars), requires_grad=True) 
+for k in range(10000):
+    ####################### FORWARD PASS ##################################
+    # hot encode the inputs
+    xenc = F.one_hot(x, num_classes=28).float()
 
-a =  xenc @ W
+    # feed the inputs into the net
+    logits =  xenc @ W # log counts
+    counts = logits.exp() # counters of next char 
+
+    # obtained probs of next char (given the input one)
+    prob = counts / counts.sum(1, keepdim=True) # [n_inputs x 28]
+
+    # Compute loss 
+    my_probs = prob[torch.arange(l), y] # get from the net the probability of the next char given the input (higher is better, since i'm using the input and the truth label)
+    loss = -my_probs.log().mean() # compute negative log like
+
+    print("NLL: ", loss.item()) 
+        
+    ####################### BACKWARD PASS ##################################
+    W.grad = None # reset gradients
+    loss.backward() 
+
+    
+    ####################### UPDATE ##################################
+    W.data += -0.5*W.grad
+
+
+
+
+
+
+
 
 
